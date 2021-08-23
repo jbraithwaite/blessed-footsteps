8d6a53c7178f2ed1e1149ec8e1f11d90bef748b4
Justin Braithwaite <justinbraithwaite@gmail.com>
2021-08-22 20:05:17 -1000

    Enable Previews

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,19 +1,33 @@
 import cx from 'classnames';
 import * as React from 'react';
+import { useRouter } from 'prismic/router';
 
 export const Container: React.FunctionComponent<ContainerProps> = ({
   children,
   className,
 }) => {
+  const { isPreview } = useRouter();
+
   return (
-    <div
-      className={cx(
-        'max-w-full px-5 py-20 sm:px-24 sm:max-w-3xl sm:mx-auto',
-        className,
+    <>
+      <div
+        className={cx(
+          'max-w-full px-5 py-20 sm:px-24 sm:max-w-3xl sm:mx-auto',
+          className,
+        )}
+      >
+        {children}
+      </div>
+      {isPreview && (
+        // eslint-disable-next-line @next/next/no-html-link-for-pages
+        <a
+          href="/api/exit-preview"
+          className="bg-yellow-400 text-white p-3 rounded-lg absolute bottom-4 right-4 shadow-md hover:shadow-sm hover:bg-yellow-500 active:shadow-inner active:bg-yellow-600"
+        >
+          Exit Preview
+        </a>
       )}
-    >
-      {children}
-    </div>
+    </>
   );
 };
 
---
+++ src/pages/api/exit-preview.ts
@@ -0,0 +1,12 @@
+import { NextApiHandler } from 'next';
+
+const exitPreview: NextApiHandler = (_, res) => {
+  // Exit the current user from "Preview Mode". This function accepts no args.
+  res.clearPreviewData();
+
+  // Redirect the user back to the index page.
+  res.writeHead(307, { Location: '/' });
+  res.end();
+};
+
+export default exitPreview;
