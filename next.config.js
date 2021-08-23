8132045f84f81a6b90d980117dacb4fdb47d1cb5
Justin Braithwaite <justinbraithwaite@gmail.com>
2021-08-23 09:42:14 -1000

    Expose PRISMIC_REPOSITORY_NAME on client

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ const withTM = require('next-transpile-modules')(['next-slicezone']);
 module.exports = withTM({
   reactStrictMode: true,
   trailingSlash: true,
+  env: {
+    PRISMIC_REPOSITORY_NAME: process.env.PRISMIC_REPOSITORY_NAME,
+  },
 });
