8fea97a1476047f069db9896f01b16aa2bf1d715
Justin Braithwaite <justinbraithwaite@gmail.com>
2021-09-06 23:33:29 -1000

    Add plausible analytics (#25)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,9 @@ class MyDocument extends Document {
   }
 
   render() {
+    const canonicalDomain = process.env.CANONICAL_DOMAIN;
+    const plausibleDomain = process.env.PLAUSIBLE_DOMAIN;
+
     return (
       <Html>
         <Head>
@@ -22,6 +25,14 @@ class MyDocument extends Document {
             defer
             src={`https://static.cdn.prismic.io/prismic.js?new=true&repo=${PRISMIC_REPOSITORY_NAME}`}
           ></script>
+          {canonicalDomain && plausibleDomain && (
+            <script
+              async
+              defer
+              data-domain={canonicalDomain.replace('www.', '')}
+              src={`https://${plausibleDomain}/js/plausible.js`}
+            ></script>
+          )}
         </Head>
         <body>
           <Main />
