2438065bb5c0fee1085dda4b110551f47c7e12ee
Justin Braithwaite <justinbraithwaite@gmail.com>
2021-09-06 23:38:42 -1000

    Style changes for quote

--- src/components/slices/Quote.tsx
+++ src/components/slices/Quote.tsx
@@ -10,7 +10,7 @@ export const Quote: React.FunctionComponent<SliceProps<QuoteSlice>> = ({
   slice,
 }) => {
   return (
-    <blockquote className="font-serif text-lg leading-loose border-l-8 py-5 pl-5 sm:pl-10 sm:max-w-4xl">
+    <blockquote className="font-serif text-lg leading-loose border-l-8 py-5 pl-5 sm:pl-10 sm:max-w-3xl">
       <RichText
         render={slice.primary.quote_content}
         htmlSerializer={htmlSerializer}
@@ -27,6 +27,6 @@ export const quoteSlice = type({
   slice_label: t.union([t.string, t.null]),
   primary: t.type({
     quote_content: richTextBlock,
-    quote_attribution: t.string,
+    quote_attribution: t.union([t.string, t.null]),
   }),
 });
