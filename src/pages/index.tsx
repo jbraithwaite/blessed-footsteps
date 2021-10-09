9bcfcd7e9174a31de7ed58d3f301c50a0b402c23
Justin Braithwaite <justinbraithwaite@gmail.com>
2021-10-08 15:07:17 -1000

    Copy update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       </Header>
       <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mb-5">
         <h2>`Abdu&apos;l-Bahá&apos;s Visit to California</h2>
-        <p className="lead">
+        <p>
           How could the Message of peace and unity of a mid-19th century Persian
           who had been imprisoned, tortured and exiled, reach the shores of the
           Pacific Ocean in the early 20th century with such an impact as to
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
         </p>
         <p>
           This question is explored by focusing on just 24 days of the 77 years
-          in the life `Abdu&apos;l-Bahá—the son appointed by
+          in the life `Abdu&apos;l-Bahá—the Son appointed by
           Bahá&apos;u&apos;lláh to maintain the purity of His Teachings after
           His passing. `Abdu&apos;l-Bahá&apos;s set out in 1911, at the age of
           67, to familiarize those living in European and American cities of the
@@ -30,14 +30,25 @@ const Home: NextPage = () => {
           promote true human happiness according to the heavenly principles and
           guidance authored by His father, Bahá’u’lláh.
         </p>
-        <i>Join us in tracing these blessed footsteps.</i>
+        <p>
+          <strong>BlessedFootsteps.org</strong> is an ever-growing website
+          serving as a repository for the many personal accounts of whose who
+          encountered `Abdu’l-Bahá while visiting California. Additionally
+          photos and newspaper articles from the time will be included.
+          Information about visiting these sacred places blessed by
+          `Abdu’l-Bahá’s footsteps will be included as well. The launch date is
+          expected to be in November 2021. Sign up for updated information about
+          the website.
+        </p>
       </div>
+
       <div className="p-5 bg-white filter drop-shadow-2xl rounded-2xl mt-10 ">
         <strong className="block text-2xl text-gray-800 mb-5">
           Receive updates
         </strong>
         <NewsletterSignup />
       </div>
+
       <footer className="text-gray-300 text-center text-xs my-10">
         © {new Date().getFullYear()} Blessed Footsteps |{' '}
         <a href="mailto:tracingsacredfootsteps@gmail.com?subject=blessedfootsteps.org">
